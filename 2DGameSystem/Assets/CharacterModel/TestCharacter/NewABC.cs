945202ba75af2afcbb45b53c3435ff438eeda8b3
3118004669侯俊希 <h36807212@163.com>
2020-10-16 17:04:20 +0800

    debug

---
+++ 2DGameSystem/Assets/CharacterModel/TestCharacter/NewABC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewABC : MonoBehaviour
+{
+    BoxCollider2D bc;
+    Rigidbody2D rb;
+    public bool isJump;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isJump = Input.GetButtonDown("JumpUp");
+    }
+    void FixedUpdate()
+    {
+        Jump();
+    }
+    void Jump()
+    {
+        if (isJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 12f);
+        }
+    }
+}
